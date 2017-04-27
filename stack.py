2fe4d2c3bbbf21d42b9588b933faaff7b80d169e
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-04-27 11:39:45 -0700

    Add module def and styled code to pydoc & pylint

--- stack.py
+++ stack.py
@@ -1,27 +1,49 @@
 #!/usr/bin/python
 
-# Stack implementation
+"""
+ Stack implementation
 
-# @Author: Kenny Iraheta
-# @Date: 2017-04-25
+ @Author: Kenny Iraheta
+ @Date: 2017-04-25
 
-# Each stack op takes O(1) time
+ Each stack op takes O(1) time
+"""
 
 class Stack:
+    """ Implementation of Stack"""
+
     def __init__(self):
+        """
+        Inits Stack with empty list
+        """
         self.stack = []
 
     def is_empty(self):
+        """
+        Returns True if Stack is empty
+        """
         return self.stack == []
 
     def push(self, item):
+        """
+        Pushes item into Stack
+        """
         self.stack.append(item)
 
     def pop(self):
+        """
+        Pops last item from Stack
+        """
         return self.stack.pop()
 
     def peek(self):
+        """
+        Returns last item from Stack
+        """
         return self.stack[-1]
 
     def size(self):
+        """
+        Returns length of Stack
+        """
         return len(self.stack)
