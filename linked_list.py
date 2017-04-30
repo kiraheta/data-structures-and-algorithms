dc18027d17223ff648dac540f1d728a248b92eb6
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-04-30 00:04:46 -0500

    Add node for linkedlist

---
+++ linked_list.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+"""
+ Singly Linked List implementation
+
+ @Author: Kenny Iraheta
+ @Date: 2017-04-29
+"""
+
+class Node:
+    """ Implementation of Node"""
+
+    def __init__(self, item=None, next=None):
+        """
+        Inits Node with empty item and next
+        """
+        self.item = item
+        self.next = next
+
+    def get_item(self):
+        """
+        Returns item from Node
+        """
+        return self.item
+
+    def get_next(self):
+        """
+        Returns next from Node
+        """
+        return self.next
+
+    def set_item(self, item):
+        """
+        Sets item in Node
+        """
+        self.item = item
+
+    def set_next(self,next):
+        """
+        Sets next in Node
+        """
+        self.next = next
