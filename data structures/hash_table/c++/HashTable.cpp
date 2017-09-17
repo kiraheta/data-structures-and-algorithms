8cebd567a181532ffea1e298a7f808a04e7330c3
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-17 01:34:47 -0700

    Implement constructor & destructor methods

--- data structures/hash_table/c++/HashTable.cpp
+++ data structures/hash_table/c++/HashTable.cpp
@@ -4,3 +4,25 @@
 //
 
 #include "HashTable.h"
+
+HashTable::HashTable(){
+
+  for (int i = 0; i < TABLE_SIZE; i++){
+      Hash_Table[i] = new item;
+      Hash_Table[i]->name = "empty";
+      Hash_Table[i]->drink = "empty";
+      Hash_Table[i]->next = NULL;
+  }
+}
+
+HashTable::~HashTable() {
+    item* Ptr;
+
+    for(int i = 0; i < TABLE_SIZE; i++){
+        while(Hash_Table[i] != NULL){
+            Ptr = Hash_Table[i];
+            Hash_Table[i] = Hash_Table[i]->next;
+            delete Ptr;
+        }
+    }
+}
