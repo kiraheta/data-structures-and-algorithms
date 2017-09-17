011966db68003132cfb500f95feb777f40f72f44
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-17 01:30:44 -0700

    Setup class method function prototypes

--- data structures/hash_table/c++/HashTable.h
+++ data structures/hash_table/c++/HashTable.h
@@ -13,7 +13,28 @@
 
 class HashTable {
 
-};
+private:
+    static const int TABLE_SIZE = 11;
 
+    struct item{
+        std::string name;
+        std::string drink;
+        item* next;
+    };
+
+    item* Hash_Table[TABLE_SIZE];
+
+public:
+
+    HashTable();
+    ~HashTable();
+    int hash(std::string key);
+    int numOfItemsInIndex(int index);
+    void printTable();
+    void printItemsInIndex(int index);
+    void findDrink(std::string name);
+    void addItem(std::string name, std::string drink);
+    void removeItem(std::string name);
+};
 
 #endif /* HashTable_h */
