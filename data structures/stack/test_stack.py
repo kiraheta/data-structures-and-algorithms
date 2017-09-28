3df8eca406c57aa6bf6aa906b33cca51c75a6c92
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-28 14:26:30 -0700

    Add test_pop unit test for Test_Stack class

--- data structures/stack/test_stack.py
+++ data structures/stack/test_stack.py
@@ -17,6 +17,11 @@ class Test_Stack(unittest.TestCase):
         s.push(5)
         self.assertEqual(1, s.size())
 
+    def test_pop(self):
+        s = Stack()
+        s.push(5)
+        self.assertEqual(5, s.pop())
+
 
 if __name__ == '__main__':
     unittest.main()
