b79550b276ed52be773aeb31012143f3c5132355
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-27 22:25:27 -0700

    Add addVertex to Graph class

--- data structures/graph/adjacencylist.py
+++ data structures/graph/adjacencylist.py
@@ -50,3 +50,9 @@ class Graph:
     def __init__(self):
         self.vertList = {}
         self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
