934c1c37dac508ef9c2a30421ddf0a888f866e3c
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-28 14:18:31 -0700

    Add test_is_empty unit test for Test_Stack class

--- data structures/stack/test_stack.py
+++ data structures/stack/test_stack.py
@@ -8,6 +8,10 @@ class Test_Stack(unittest.TestCase):
         s = Stack()
         self.assertEqual(0, s.size())
 
+    def test_is_empty(self):
+        s = Stack()
+        self.assertEqual(0, s.size())
+
 
 if __name__ == '__main__':
     unittest.main()
