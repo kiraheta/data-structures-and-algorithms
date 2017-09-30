2a8f443a0897694c0fbc20b33035abc05ebea635
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-30 01:23:28 -0700

    Add test_init unit test for Test_BinarySearchTree class

--- data structures/BST/test_binary_search_tree.py
+++ data structures/BST/test_binary_search_tree.py
@@ -2,10 +2,13 @@ from binary_search_tree import BinarySearchTree
 import unittest
 
 
-class Test_BinarySearchTree():
+class Test_BinarySearchTree(unittest.TestCase):
 
     def setUp(self):
         self.b = BinarySearchTree()
+
+    def test_init(self):
+        self.assertEqual(0, self.b.size)
 
 
 if __name__ == '__main__':
