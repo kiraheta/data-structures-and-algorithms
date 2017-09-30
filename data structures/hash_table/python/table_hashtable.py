cdcbea4e1a414bf5c6c3691603de0a01eadc6872
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-30 00:51:03 -0700

    Add test_init unit test for Test_HashTable class

--- data structures/hash_table/python/table_hashtable.py
+++ data structures/hash_table/python/table_hashtable.py
@@ -5,7 +5,10 @@ import unittest
 class Test_HashTable(unittest.TestCase):
 
     def setUp(self):
-        self.h = Hashtable()
+        self.h = HashTable()
+
+    def test_init(self):
+        self.assertEqual(11, self.h.size)
 
 
 if __name__ == '__main__':
