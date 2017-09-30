0ba22958a6b8191595105e99438c2af730bb7fa3
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-30 01:10:02 -0700

    Add test_get unit test for Test_HashTable class

--- data structures/hash_table/python/table_hashtable.py
+++ data structures/hash_table/python/table_hashtable.py
@@ -20,6 +20,10 @@ class Test_HashTable(unittest.TestCase):
     def test_rehash(self):
         self.assertEqual(2, self.h.rehash(self.h.hashfunction(78, 11), 11))
 
+    def test_get(self):
+        self.h[22] = "cat"
+        self.assertEqual("cat", self.h.get(22))
+
 
 if __name__ == '__main__':
     unittest.main()
