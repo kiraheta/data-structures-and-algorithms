8f7f6d835d5c682775786fdaff780a97bb0688fa
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-09-29 22:27:08 -0700

    Add test_init unit test for Test_LinkedList class

--- data structures/linkedlist/test_linked_list.py
+++ data structures/linkedlist/test_linked_list.py
@@ -2,10 +2,13 @@ from linked_list import LinkedList
 import unittest
 
 
-class Test_LinkedList():
+class Test_LinkedList(unittest.TestCase):
 
     def setUp(self):
         self.l = LinkedList()
+
+    def test_init(self):
+        self.assertEqual(0, self.l.size())
 
 
 if __name__ == '__main__':
