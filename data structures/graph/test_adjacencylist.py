413f6a013a9d5f4b4cc36ef8cc835a229581b5ff
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-01 02:11:44 -0700

    Add test_addVertex unit test for Test_Graph class

--- data structures/graph/test_adjacencylist.py
+++ data structures/graph/test_adjacencylist.py
@@ -10,6 +10,10 @@ class Test_Graph(unittest.TestCase):
     def test_init(self):
         self.assertEqual(0, self.g.numVertices)
 
+    def test_addVertex(self):
+        self.g.addVertex(22)
+        self.assertEqual(1, self.g.numVertices)
+
 
 if __name__ == '__main__':
     unittest.main()
