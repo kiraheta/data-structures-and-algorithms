d676b1c07f82aea894e51f2b505f3785b5918214
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-12 23:35:38 -0700

    code: implement bubble sort

---
+++ sort/bubblesort.py
@@ -0,0 +1,20 @@
+#!/bin/usr/python
+
+"""
+Bubblesort Implementation
+"""
+
+
+def bubblesort(aList):
+    for i in range(len(aList) - 1):
+        for j in range(len(aList) - 1):
+            if aList[j] > aList[j + 1]:
+                aList[j], aList[j + 1] = aList[j + 1], aList[j]
+
+
+if __name__ == '__main__':
+
+    aList = [44, 40, 30, 4, 2, 6, 1, 11, 8]
+
+    bubblesort(aList)
+    print(aList)
